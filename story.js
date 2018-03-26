9c65be62424adada6add3358e2e1a73e4d56505d
jdz <jdz@1.com>
2018-03-26 16:40:54 +0800

    initial story

---
+++ story.js
@@ -0,0 +1,7 @@
+let given="Given:stand in the front of the door";
+let when1="When: ask Open the door";
+let when2="When: I am Mom";
+let then="Then: open the door"
+
+let arrayStory=[given,when1,when2,then];
+console.log(arrayStory);
