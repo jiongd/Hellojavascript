0d1cbfc853ffc46e3eeafb488d3307834b51dc20
jdz <jdz@1.com>
2018-04-08 13:47:18 +0800

    complete master

--- LoginStory.mjs
+++ LoginStory.mjs
@@ -9,22 +9,26 @@ export default class LoginStory extends story {
        this.expected;
        this.actual;
    }
+  GetKey(context){
+     const value=context.substring(context.indexOf("[")+1,context.indexOf("]"));
+     return value;
+  }
   When(context){
       super.When(context);
       // 使用正则表达式，\s表示空格, + 表示一个或多个空格，[]在正则表达式中需要进行转义，用\转义；
       if(/enter\s+user\s+name\s+\[admin\]/.test(context)){
-         this.username=context.substring(context.indexOf("[")+1,context.indexOf("]"));
+         this.username=this.GetKey(context);
          console.log(`The username is ${this.username}`);
        }
       if(/enter\s+password\s+\[taylor2018\]/.test(context)){
-          this.password=context.substring(context.indexOf("[")+1,context.indexOf("]"));       
+          this.password=this.GetKey(context);       
           console.log(`The password is ${this.password}`);
       }
   }
  Then(context){
      super.Then(context);
      if(/\[successful!\]/.test(context)){
-     this.expected=context.substring(context.indexOf("[")+1,context.indexOf("]"));
+     this.expected=this.GetKey(context);
      console.log(`expected result is ${this.expected}`);
      }
      let driver=new webdriver.Builder().forBrowser("chrome").build();
